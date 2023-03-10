871feeedc9d792a06fff2a573616229a368af352
kennedyisn <kennedyhayles@gmail.com>
2023-03-09 19:49:12 -0500

    Final application edits

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,11 @@
+var formInputEl = document.querySelector('#form');
 var displayedTimer = document.querySelector('#timer');
 var quizStartScreen = document.querySelector('#quizstartscreen');
 var savedscores = document.querySelector("#highscores");
 var quizTime = 120;
+var currentQuestion = -1;
 var highScores = [];
-var currentQuestion = 0;
-
+var score = 0;
 var startbtn = document.getElementById("btn");
 startbtn.addEventListener("click", function(event) {
   event.preventDefault();
@@ -15,17 +16,21 @@ startbtn.addEventListener("click", function(event) {
 })
 
 function timerFunc() {
-  timerId = setInterval(timerFunc, 1000)
-  displayedTimer.textContent = quizTime;
-  quizTime--;
-  displayedTimer.textContent = ('Time remaining:'+ quizTime);
+  timerId = setInterval(function() {
+    displayedTimer.textContent = ('Time remaining: ' + quizTime);
+    quizTime--;
+    if (quizTime <= 0) {
+      clearInterval(timerId);
+      document.getElementById('timer').style.visibility='hidden';
+      endOfQuiz();
+    }
+  }, 1000);
 }
 
 function startQuiz() {
-  var quizStartScreen = document.querySelector('#quizstartscreen');
   document.getElementById('btn').style.visibility='hidden';
   document.getElementById('text').style.visibility='hidden';
-  currentQuestion = 0;
+  currentQuestion++;
   score = 0;
   showQuestion();
 };
@@ -33,25 +38,58 @@ function startQuiz() {
 // Define the quiz questions and answers
 const quizQuestions = [
   {
-    question: 'What does blank mean?',
+    question: 'What does CSS stand for?',
     answers: [
-      'not blank',
-      'definietly not blank',
-      'blank',
-      '7', 
+      'Computer Styling Software',
+      'Computer Screen Styling',
+      'Central Software System',
+      'Cascading Style Sheets', 
     ],
-    correct: 'blank'
+    correct: 'Cascading Style Sheets'
   },
 
   {
-    question: 'What day is it?',
+    question: 'Which of the following is not an API type?',
     answers: [
-      'not today',
-      'yesterday',
-      'tomorrow',
-      'today', 
+      'public',
+      'private',
+      'composite',
+      'regulatory', 
     ],
-    correct: 'today'
+    correct: 'regulatory'
+  },
+
+  {
+    question: 'In the statement "var Apple=47;" what is "Apple"?',
+    answers: [
+      'Variable name',
+      'Value',
+      'event listener',
+      'Fruit', 
+    ],
+    correct: 'Variable name'
+  },
+
+  {
+    question: 'What does HTML stand for?',
+    answers: [
+      'Hot Typing MoneyLine',
+      'High Tension Megabyte Language',
+      'HyperText Markup Language',
+      'nothing', 
+    ],
+    correct: 'HyperText Markup Language'
+  },
+
+  {
+    question: 'What is Var used for?',
+    answers: [
+      'empty a <div> tag',
+      'to store information to be referenced and manipulated',
+      'tomorrow',
+      'empty a local storage', 
+    ],
+    correct: 'to store information to be referenced and manipulated'
   },
 ];
 
@@ -62,36 +100,115 @@ function showQuestion() {
   console.log(question1)
   $('#cardheader').text(question1);
   $('#answers').empty();
+  var correctPick = question.correct
   for (var i = 0; i <question.answers.length; i++) {
-      var answerBtn = $('<button>', {
+    console.log(question)  
+    var answerBtn = $('<button>', {
       text: question.answers[i],
       click: function () {
-        
+        grader($(this),correctPick)
+        displayNextQuestion()
       }
     })
     $('#answers').append(answerBtn);
   };
 };
 
-function displayNextQuestion() {
+function grader(clickedButton, correctPick) {
+  var answerChoice = clickedButton.text();
+  var correctChoice = correctPick;
+  if (answerChoice ===correctChoice) {
+    score++
+  }
+  else {
+    timeLeft -= 10;
+  }
+}
 
+function displayNextQuestion() {
+  currentQuestion ++;
+  console.log(currentQuestion)
+  if (currentQuestion > 4){
+    endOfGame()
+  }
+  else {
+    console.log(currentQuestion)
+    var questionBlank = quizQuestions[currentQuestion];
+    var thisQuestion = questionBlank.question
+    console.log(thisQuestion)
+    $('#cardheader').text(thisQuestion);
+    $('#answers').empty();
+    var correctPick = questionBlank.correct
+    for (var i = 0; i <questionBlank.answers.length; i++) {
+      console.log(currentQuestion)  
+      var answerBtn = $('<button>', {
+        text: questionBlank.answers[i],
+        id: "option",
+        click: function () {
+          grader($(this), correctPick);
+          displayNextQuestion()
+        }
+      })
+      $('#answers').append(answerBtn);
+    };
+  }
 }
 
 function endOfGame () {
-
+  finalScore = score + quizTime;
+  console.log(finalScore)
+  //calculate score and make it a var to go into header
+  document.getElementById('option').style.visibility='hidden';
+  document.getElementById('timer').style.visibility='hidden';
+  $('#answers').empty();
+  $('#cardheader').text('You have completed the quiz! your score is :' + finalScore);
+  //ending text
+  $('#text').text('Enter your initials below to save your score.');
+  //create form
+  var initialsForm = $('<input>', {
+    type: 'text',
+    id: 'initials-input',
+    name: 'form',
+  })
+  $('#form').append(initialsForm);
+  //create submit button(onclick it saves to local storage array)
+  var submitBtn = $('<button>', {
+    text: "Submit",
+    click: function () {
+    // add Final score and initials to localstorage
+    saveScore(finalScore)
+    displayHighscores()  
+    }
+  })
+  submitBtn.on('click', function(event) {
+    event.preventDefault();
+    saveScore();
+  });
+  $('#answers').append(submitBtn);
 }
   
+function saveScore(value) {
+  var initialsInput = document.querySelector('#initials-input');
+  var userInitials = initialsInput.value;
+  let scoreObj = {
+    name: userInitials,
+    value: value
+  };
+  highScores.push(scoreObj);
+  localStorage.setItem('scores', JSON.stringify(highScores));
+}
+
 function displayHighscores() {
   // Clear highscores and update with new score by user
-  savedscores.innerHTML = "";
+  $(savedscores).empty();
   // Render a new li for high score
   for (var i = 0; i < highScores.length; i++) {
-    var initials = highScores[i];
+    var initials = highScores[i].name;
     var score = highScores[i].value;
 
     var newscores = $('<li>')
       .text(initials + ' Score:' +score)
       .addClass('li')
-    savedscores.appendChild(newscores);
+    $(savedscores).append(newscores);
   }
 }
